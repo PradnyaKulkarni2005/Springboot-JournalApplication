56dbb67558343a899a80bb1303383e43445d4499
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-03-03 09:53:30 +0530

    added put , post , delete apis

--- src/main/java/com/example/journal/JournalApplication.java
+++ src/main/java/com/example/journal/JournalApplication.java
@@ -1,15 +1,13 @@
 package com.example.journal;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class JournalApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context=SpringApplication.run(JournalApplication.class, args);
-		Dev dev = context.getBean(Dev.class);
-		dev.build();
+		SpringApplication.run(JournalApplication.class, args);
+		
+		
 	}
 }
--- src/main/java/com/example/journal/Laptop.java
+++
@@ -1,8 +0,0 @@
-package com.example.journal;
-import org.springframework.stereotype.Component;
-@Component
-public class Laptop {
-    public void compile(){
-        System.out.println("Compiling the laptop code...");
-    } 
-}
